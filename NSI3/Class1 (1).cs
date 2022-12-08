f007ac8bf5936ee1a966206de6f73910b7c62b69
PawelKolawa <73112206+PawelKolawa@users.noreply.github.com>
2022-12-08 11:51:00 +0100

    Add files via upload

---
+++ NSI3/Class1 (1).cs
@@ -0,0 +1,79 @@
+﻿using Calculator.ConsoleApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace KalkulatorTest.UnitTests
+{
+    public class KalkulatorTest
+    {
+        [Fact]
+        public void sprawdzaniedodawania() 
+        {
+            //arrange
+            var kalkulator = new Program();
+            //act
+            double wynik = kalkulator.dodawanie(1, 2);
+            //assert
+            Assert.Equal(3, wynik);
+        }
+
+        [Fact]
+        public void sprawdzanieodejmowania()
+        {
+            //arrange
+            var kalkulator = new Program();
+            //act
+            double wynik = kalkulator.odejmowanie(5, 2);
+            //assert
+            Assert.Equal(3, wynik);
+        }
+
+        [Fact]
+        public void sprawdzaniemnozenia()
+        {
+            //arrange
+            var kalkulator = new Program();
+            //act
+            double wynik = kalkulator.mnozenie(3, 2);
+            //assert
+            Assert.Equal(6, wynik);
+        }
+
+        [Fact]
+        public void sprawdzaniedzielenia()
+        {
+            //arrange
+            var kalkulator = new Program();
+            //act
+            double wynik = kalkulator.dzielenie(6, 2);
+            //assert
+            Assert.Equal(3, wynik);
+        }
+
+        [Fact]
+        public void sprawdzanieresztazdzielenia()
+        {
+            //arrange
+            var kalkulator = new Program();
+            //act
+            double wynik = kalkulator.reszta_z_dzielenia(9, 2);
+            //assert
+            Assert.Equal(1, wynik);
+        }
+
+        [Fact]
+        public void sprawdzaniepotegowania()
+        {
+            //arrange
+            var kalkulator = new Program();
+            //act
+            double wynik = kalkulator.potega(2, 3);
+            //assert
+            Assert.Equal(8, wynik);
+        }
+    }
+}
---
+++ NSI3/Program (1).cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Calculator.ConsoleApp
+{
+    public class Program
+    {
+        static void Main() 
+        {
+            Console.WriteLine(7 % 2);
+        }
+        public double dodawanie(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double odejmowanie(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double mnozenie(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double dzielenie(double a, double b)
+        {
+            double y = 0;
+            if (b == 0)
+            {
+                Console.WriteLine("Nie dzielimy przez 0");
+            }            
+            else
+            {
+               
+                y = a / b;
+            }
+            return y;
+        }
+
+        public double reszta_z_dzielenia(double a, double b)
+        {
+            double y = 0;
+            if (b == 0)
+            {
+                Console.WriteLine("Nie dzielimy przez 0");
+            }
+            else
+            {
+
+                y = a % b;
+            }
+            return y;
+        }
+
+        public double potega(double a, double b)
+        {
+            double wynik=1;
+            while (b > 0)
+            {
+                wynik *= a;
+                b--;
+            }
+            return wynik;
+        }
+
+    }
+}
